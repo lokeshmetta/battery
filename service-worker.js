10aa4cc7554f1888a0037a805180d8e731d20667
Lokesh Metta <26522232+lokeshmetta@users.noreply.github.com>
2023-12-06 09:41:53 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,66 @@
+const CACHE_NAME = 'battery-alert-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/your-other-files.css',
+  '/your-scripts.js',
+  '/your-images.png',
+  '/your-sound-file.mp3', // Include your sound file here if needed
+  // Add other files that you want to cache for offline use
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request to use it both in cache and fetch
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest)
+          .then(response => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response to cache it
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          return cacheName !== CACHE_NAME;
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
